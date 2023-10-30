db5afe6e32d4435f6b0b9cfa30a19dbfaff1d3ea
aeither <36173828+aeither@users.noreply.github.com>
2023-10-30 16:37:43 +0300

    theme, button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
