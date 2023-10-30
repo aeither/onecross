70a7a8782c3c6bec7e77dd18edf086507925a441
aeither <36173828+aeither@users.noreply.github.com>
2023-10-30 18:01:57 +0300

    web3 modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,44 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 
 import "@/styles/globals.css";
 
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
+
+import { WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon, polygonMumbai } from "wagmi/chains";
+
+// 1. Get projectId
+const projectId = "42296b89291f363670cdc66378b83a15";
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "App",
+  description: "App Example",
+  url: "https://web3modal.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, arbitrum, polygon, polygonMumbai];
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <Component {...pageProps} />
-      <Toaster />
-    </ThemeProvider>
+    <WagmiConfig config={wagmiConfig}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Component {...pageProps} />
+        <Toaster />
+      </ThemeProvider>
+    </WagmiConfig>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,44 @@
 import { Button } from "@/components/ui/button";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { Signer, ethers } from "ethers";
+import { useState } from "react";
 
 export default function Home() {
+  const [connectedAddress, setConnectedAddress] = useState("");
+  const [signer, setSigner] = useState<Signer>();
+
+  const { open } = useWeb3Modal();
+
+  const connectWallet = async () => {
+    const windowEthereum = (window as unknown as any).ethereum;
+    if (typeof windowEthereum !== "undefined") {
+      try {
+        // Request account access if needed
+        await windowEthereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        // Create an ethers provider instance
+        const provider = new ethers.providers.Web3Provider(windowEthereum);
+
+        // Get the selected account address
+        const accounts = await provider.listAccounts();
+        const selectedAddress = accounts[0];
+
+        // Update the connected address state
+        selectedAddress && setConnectedAddress(selectedAddress);
+
+        // Set the signer
+        const connectedSigner = provider.getSigner();
+        setSigner(connectedSigner);
+      } catch (error) {
+        console.error("Error connecting to wallet:", error);
+      }
+    } else {
+      console.error("Please install MetaMask to connect your wallet.");
+    }
+  };
+
   return (
     <>
       <main className=" flex min-h-[calc(100vh-64px)] w-full flex-col">
@@ -8,6 +46,17 @@ export default function Home() {
           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
             Heading
           </h1>
+          <Button variant={"default"} onClick={() => open()}>
+            Open Connect Modal
+          </Button>
+
+          <Button
+            onClick={() => {
+              connectWallet();
+            }}
+          >
+            connectWallet
+          </Button>
           <Button
             onClick={() => {
               alert("hello");
